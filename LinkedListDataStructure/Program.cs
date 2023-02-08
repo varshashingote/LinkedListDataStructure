a4bcbee1250ca4c94fd0e46cae4fd9a1f14fdb54
varsha Gunjal <varshashingote01@gmail.com>
2023-02-08 22:55:23 +0530

    [varsha] Welcome to LinkedList

---
+++ LinkedListDataStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Linked List DataStructure!!!");
+            Console.ReadLine();
+        }
+    }
+}
