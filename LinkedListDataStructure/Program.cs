0d83100be093ab246825a796c032c5fb892fa0d1
varsha Gunjal <varshashingote01@gmail.com>
2023-02-09 12:59:22 +0530

    [varsha] Append method

--- LinkedListDataStructure/Program.cs
+++ LinkedListDataStructure/Program.cs
@@ -12,19 +12,23 @@ namespace LinkedListDataStructure
         {
             Console.WriteLine("Welcome To Linked List DataStructure!!!");
             CustomLinkedList customLinkedList = new CustomLinkedList();
-           //customLinkedList.Add(56);
-           // customLinkedList.Add(30);
-           // customLinkedList.Add(70);
-           // customLinkedList.Display();
-            customLinkedList.AddFirst(70);
-            customLinkedList.AddFirst(30);
-            customLinkedList.AddFirst(56);
+            //customLinkedList.Add(56);
+            //customLinkedList.Add(30);
+            //customLinkedList.Add(70);
+            //customLinkedList.Display();
+            //customLinkedList.AddFirst(70);
+            //customLinkedList.AddFirst(30);
+            //customLinkedList.AddFirst(56);
+            //customLinkedList.Display();
+            //Console.ReadLine();
+            customLinkedList.Append(56);
+            customLinkedList.Append(30);
+            customLinkedList.Append(70);
             customLinkedList.Display();
-
-
             Console.ReadLine();
 
-        
+
+
         }
     }
 }
